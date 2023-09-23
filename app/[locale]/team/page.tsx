957ab2859594ec3fa74aa65246f04820c36d023c
David Bartal <bartaldvd@gmail.com>
2023-09-23 17:03:35 +0200

    team page long description

--- app/[locale]/team/page.tsx
+++ app/[locale]/team/page.tsx
@@ -1,14 +1,21 @@
 import CollectionPage from "@/components/CollectionPage";
 import ProfilePicture from "@/components/ProfilePicture";
-import { getResearchers } from "@/lib/contentful/setup";
+import { getResearchers, getSiteDetails } from "@/lib/contentful/setup";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { useLocale, useTranslations } from "next-intl";
 import LinkWithRef from "next-intl/link";
 
 async function TeamPage() {
   const locale = useLocale();
   const researchers = await getResearchers(locale);
+  const {
+    fields: { longDescription },
+  } = await getSiteDetails(locale);
   return (
     <CollectionPage titleId="team">
+      <div className="prose max-w-prose">
+        {documentToReactComponents(longDescription)}
+      </div>
       {researchers.items.map((researcher) => (
         <LinkWithRef
           className="bg-white rounded-lg p-4 shadow-sm w-full md:w-1/3 hover:shadow-md flex gap-2 items-center "
--- components/NavLink.tsx
+++ components/NavLink.tsx
@@ -12,7 +12,7 @@ export function NavLink({ href, label }: { href: string; label: string }) {
       href={href}
       className={`${
         isActive ? "font-semibold bg-slate-100" : ""
-      } w-full sm:w-auto hover:bg-slate-100 p-2 rounded hover:shadow-md transition-all ease-in-out`}
+      } w-full sm:w-auto hover:bg-slate-50 p-2 rounded hover:shadow-md transition-all ease-in-out`}
     >
       {label}
     </Link>

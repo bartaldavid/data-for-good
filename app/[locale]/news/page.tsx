17a29b602550e6971c5dabb0c227072608764835
Dávid Bartal <35382618+bartaldavid@users.noreply.github.com>
2023-09-26 12:51:57 +0000

    meta-title on news page

--- app/[locale]/news/page.tsx
+++ app/[locale]/news/page.tsx
@@ -1,6 +1,17 @@
 import CollectionItem from "@/components/CollectionItem";
 import CollectionPage from "@/components/CollectionPage";
 import { getNews } from "@/lib/contentful/setup";
+import { ResolvingMetadata } from "next";
+import { useTranslations } from "next-intl";
+
+export async function generateMetadata(
+  { params }: { params: { locale: string } },
+  parent: ResolvingMetadata
+) {
+  const title = params?.locale && params.locale === "hu" ? "Hírek" : "News";
+  const parentTitle = (await parent).title?.absolute || "";
+  return { title: `${title} | ${parentTitle}` };
+}
 
 async function NewsCollectionPage({
   params: { locale },
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Data for Good'
+}
+
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang={"en"}>

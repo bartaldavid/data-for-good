e5301169cd4f65d34192cf3acbde76b7bbd9f509
Dávid Bartal <35382618+bartaldavid@users.noreply.github.com>
2023-11-02 23:55:18 +0100

    restore projects

---
+++ app/[locale]/projects/[slug]/page.tsx
@@ -0,0 +1,18 @@
+import BlogPageLayout from "@/components/BlogPage";
+import DocumentWithImages from "@/components/DocumentWithImages";
+import { getProjects } from "@/lib/contentful/setup";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { getTeachingPage } from "@/lib/contentful/setup";
+import { useLocale } from "next-intl";
+
+async function SingleProjectPage({ params }: { params: { slug: string } }) {
+  const locale = useLocale();
+  const project = (await getProjects(locale, params.slug))["items"][0];
+  return (
+    <BlogPageLayout title={project.fields.title} pubDate={project.fields.date}>
+      <DocumentWithImages document={project.fields.content} />
+    </BlogPageLayout>
+  );
+}
+
+export default SingleProjectPage;
---
+++ app/[locale]/projects/page.tsx
@@ -0,0 +1,33 @@
+import CollectionItem from "@/components/CollectionItem";
+import CollectionPage from "@/components/CollectionPage";
+import { getProjects } from "@/lib/contentful/setup";
+import { getTeachingPage } from "@/lib/contentful/setup";
+import { ResolvingMetadata } from "next";
+import { useLocale } from "next-intl";
+
+export async function generateMetadata(
+  { params }: { params: { locale: string } },
+  parent: ResolvingMetadata
+) {
+  const title = params?.locale === "hu" ? "Projektek" : "Projects";
+  const parentTitle = (await parent).title?.absolute || "";
+  return { title: `${title} | ${parentTitle}` };
+}
+
+async function ProjectsPage() {
+  const locale = useLocale();
+  const projects = await getProjects(locale);
+
+  return (
+    <CollectionPage titleId="projects">
+      {projects.items.map((project) => (
+        <CollectionItem
+          title={project.fields.title}
+          date={project.fields.date}
+          key={project.fields.slug}
+          href={`/projects/${project.fields.slug}`}
+        />
+      ))}
+    </CollectionPage>
+  );
+}
